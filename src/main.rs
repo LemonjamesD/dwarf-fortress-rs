2c0906e7618bbeae83e9420c249bb2ad8df21fab
lonnonjamesd <lemonjameslem@protonmail.com>
2022-04-12 15:46:50 -0400

    Added Started code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use winit::{window::WindowBuilder, event_loop::{EventLoop, ControlFlow}, event::{WindowEvent, ElementState, KeyboardInput, VirtualKeyCode, Event}};
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        },
+        _ => {}
+    });
 }
